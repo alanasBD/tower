5380050cda36789834819c3da68d103d27abc08d
alanasBD <alanas7171@gmail.com>
2022-12-12 15:15:22 +0600

    tower of hanoi updated

--- index.js
+++ index.js
@@ -11,55 +11,66 @@ let tower =
 // disk2 = 11;
 // disk3 = 12;
 
+let tower2 = [[],[],[]];
 
-let disk = 12;
+//replace Number by disk name
 
-// all disks
+const replaceNumber = () =>{
+  for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
 
-function disk1(event){
-  const diskOne = 10;
+       if(tower[i][j]===10){
+         tower2[i][j] = "disk1"
+       }
+       else if(tower[i][j] === 11){
+         tower2[i][j] = "disk2"
+       }
+       else if(tower[i][j] === 12){
+         tower2[i][j] = "disk3"
+       }
+       else{
+         tower2[i][j] = ""
+       }
 
-  if(diskOne >= disk){
-    disk = diskOne;
+    }
   }
-  
-  console.log(disk);
-  event.stopPropagation();
-
 }
 
-function disk2(event){
-  const diskTwo = 11
-
-  if(diskTwo >= disk){
-    disk = diskTwo;
-  }
-  console.log(disk);
-  event.stopPropagation();
-}
-
-function disk3(event){
-  const  diskThree = 12;
-
-  if(diskThree>= disk){
-     disk = diskThree
-  }
-  console.log(disk);
-
-  event.stopPropagation();
-}
 
 // search disk location
 
 const searchLocation =(val)=>{
-   for(let i=0;i<3;i++){
-    for(let j=0;j<3;j++){
+  for(let i=0;i<3;i++){
+   for(let j=0;j<3;j++){
       if(tower[i][j] === val){
         return [i,j];
-      }
-    }
+     }
    }
+  }
 }
+
+
+
+
+// all disks
+let disk = 12;
+
+
+
+function diskInfo (event){
+   const diskName = event.target.classList[0];
+   if(diskName === "disk1"){
+     disk = 10;
+   }
+   else if(diskName === "disk2"){
+     disk = 11;
+   }
+   else if(diskName === "disk3"){
+     disk = 12;
+   }
+
+}
+
 
 
 const  towerArea = () =>{
@@ -67,13 +78,13 @@ const  towerArea = () =>{
   game.innerHTML =  game.innerHTML = `<div class="tower-area" id="tower-area1" onclick="towerAreaOne()">
   <div class="tower">
       <div style="margin-top:130px">  
-          <div class="${tower[0][0]}" onclick="disk1(event)"></div>
+          <div class="${tower2[0][0]}" onclick="diskInfo(event)"></div>
       </div>
       <div  style="margin-top:-40px">
-          <div class="${tower[0][1]}"  onclick="disk2(event)"></div>
+          <div class="${tower2[0][1]}"  onclick="diskInfo(event)"></div>
       </div>
       <div style="margin-top:-40px">
-           <div class="${tower[0][2]}" onclick="disk3(event)"></div>
+           <div class="${tower2[0][2]}" onclick="diskInfo(event)"></div>
       </div>
      
   </div>
@@ -81,13 +92,13 @@ const  towerArea = () =>{
 <div class="tower-area" id="tower-area2" onclick="towerAreaTwo()">
   <div class="tower">
       <div style="margin-top:130px">  
-          <div class="${tower[1][0]}" onclick="disk1(event)"></div>
+          <div class="${tower2[1][0]}" onclick="diskInfo(event)"></div>
       </div>
       <div  style="margin-top:-40px">
-          <div class="${tower[1][1]}"  onclick="disk2(event)"></div>
+          <div class="${tower2[1][1]}"  onclick="diskInfo(event)"></div>
       </div>
       <div style="margin-top:-40px">
-           <div class="${tower[1][2]}" onclick="disk3(event)"></div>
+           <div class="${tower2[1][2]}" onclick="diskInfo(event)"></div>
       </div>
      
   </div>
@@ -95,13 +106,13 @@ const  towerArea = () =>{
 <div class="tower-area" id="tower-area3" onclick="towerAreaThree()">
   <div class="tower">
       <div style="margin-top:130px">  
-          <div class="${tower[2][0]}" onclick="disk1(event)"></div>
+          <div class="${tower2[2][0]}" onclick="diskInfo(event)"></div>
       </div>
       <div  style="margin-top:-40px">
-          <div class="${tower[2][1]}"  onclick="disk2(event)"></div>
+          <div class="${tower2[2][1]}"  onclick="diskInfo(event)"></div>
       </div>
       <div style="margin-top:-40px">
-           <div class="${tower[2][2]}" onclick="disk3(event)"></div>
+           <div class="${tower2[2][2]}" onclick="diskInfo(event)"></div>
       </div>
      
   </div>
@@ -114,30 +125,84 @@ const  towerArea = () =>{
 
 const towerAreaOne = () =>{
    const [area,index] = searchLocation(disk);
-   const item = tower[area][index];
-   tower[area].pop(item);
-   tower[0].push(item);
-   console.log(tower);
+  
+   
+  
+   const len = tower[area].length;
+   const isLastElement = tower[area][len-1] === disk ? true : false ;
+
+   const lenOfAreaOne = tower[0].length;
+
+   if(isLastElement && !lenOfAreaOne){
+    tower[area].pop();
+    tower[0].push(disk);
+  }
+  else if(isLastElement && ( disk > tower[0][lenOfAreaOne-1])){
+    tower[area].pop();
+    tower[0].push(disk);
+  }
+
+  
+
+
+   replaceNumber();
    towerArea()
 }
 
 const towerAreaTwo = () =>{
    const [area,index] = searchLocation(disk);
-   const item = tower[area][index];
-   tower[area].pop(item);
-   tower[1].push(item);
-    console.log(tower);
+
+
+
+   const len = tower[area].length;
+   const isLastElement = tower[area][len-1] === disk ? true : false ;
+
+   const lenOfAreaTwo = tower[1].length;
+
+   if(isLastElement && !lenOfAreaTwo){
+     tower[area].pop();
+     tower[1].push(disk);
+   }
+   else if(isLastElement && ( disk > tower[1][lenOfAreaTwo-1])){
+     tower[area].pop();
+     tower[1].push(disk);
+   }
+   
+   
+  
+
+   replaceNumber();
    towerArea()
+
 }
 
 
 
 const towerAreaThree = () =>{
   const [area,index] = searchLocation(disk);
-  const item = tower[area][index];
-  tower[area].pop(item);
-  tower[2].push(item);
-  console.log(tower);
+  
+
+
+  const len = tower[area].length;
+  const isLastElement = tower[area][len-1] === disk ? true : false ;
+
+  const lenOfAreaThree = tower[2].length;
+
+  if(isLastElement && !lenOfAreaThree){
+    tower[area].pop();
+    tower[2].push(disk);
+  }
+  else if(isLastElement && ( disk > tower[2][lenOfAreaThree-1])){
+    tower[area].pop();
+    tower[2].push(disk);
+  }
+
+
+  
+
+ 
+  replaceNumber();
   towerArea()
 }
+
 
